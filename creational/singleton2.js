47b04969dce48aaf9d22ee7b91bdb0d2fea1152a
Zach <zach@hackazach.net>
2012-02-25 17:45:50 -0500

    singleton2 example

---
+++ creational/singleton2.js
@@ -0,0 +1,8 @@
+//Another way of creating a Singleton could be simply creating an object literal with various functions
+//and fields. Because objects are not 'instantiated' in JS like other languages, this will also
+//work as a Singleton.
+
+singleton = { some_func: function(){ console.log('Hello there!')} ,
+			 some_data: {instance: 'dev' , public_key:'AHHHJJJJJJJDDDDHHHH1111'}
+}
+
