1415762501dc7f24799da95cc06df0e042155cf4
Zach <zach@hackazach.net>
2012-02-27 20:42:34 -0500

    more documentation in mixin

--- creational/mixin.js
+++ creational/mixin.js
@@ -6,7 +6,7 @@ var big_button = function(radius, label, action) {
 
 var my_mixin = function(){
 	if(this.hasOwnProperty('explosion')){
-	throw "Namespace explosions already exists on this object!";
+	throw "Namespace 'explosion' already exists on this object!";
 	}
 
 	this.explosion = {
@@ -17,8 +17,10 @@ var my_mixin = function(){
 
 big_red_button = new big_button;
 //mixin this stuff!
-//call my_mixin function with the 'this' object set to big_red_button.prototype.
-//this now creates an 'explosion' namespace on big_button.
+//call my_mixin function with the 'this' object set to big_red_button.
+//this now creates an 'explosion' namespace on big_red_button with two functions.
+//we use the namespace 'explosion' the minimize the chance of colliding with function names
+//already defined on big_red_button.
 my_mixin.call(big_red_button);
 
 //scheme for using this pattern:
